ae7f0ceafb7453c2e65e3cb71e5822af617c9566
mefive <mark.liu.cong@gmail.com>
2016-02-05 15:39:55 +0800

    Modal

--- src/Modal.js
+++ src/Modal.js
@@ -1,13 +1,148 @@
-import React from 'react';
+import React, {PropTypes} from 'react';
+import ReactDOM, {
+    unstable_renderSubtreeIntoContainer as renderSubtreeIntoContainer,
+    unmountComponentAtNode,
+    findDOMNode
+} from 'react-dom';
 
-const Modal = React.creactClass({
+const PinCenter = React.createClass({
+    getInitialState() {
+        return {
+            marginLeft: 0,
+            marginTop: 0  
+        };
+    },
+
+    componentDidMount() {
+        this.componentDidUpdate();
+    },
+
+    componentDidUpdate(prevProps, prevState) {
+        const element = findDOMNode(this);
+
+        const marginLeft = -(element.offsetWidth / 2);
+        const marginTop = -(element.offsetHeight / 2);
+
+        if (!prevState
+            || (marginTop !== prevState.marginTop
+            || marginLeft !== prevState.marginLeft)
+        ) {
+            this.setState({
+                marginLeft,
+                marginTop
+            });
+        }
+    },
+
     render() {
+        const {children} = this.props;
+        const {marginLeft, marginTop} = this.state;
+
+        let style = children.props.style;
+
+        style = {
+            ...style,
+            marginLeft,
+            marginTop
+        };
+        
+        return React.cloneElement(this.props.children, { style });
+    }
+});
+
+const Modal = React.createClass({
+    getDefaultProps() {
+        return {
+            visible: false,
+            maskClassName: 'modal-mask',
+            getModalContainer: null,
+            activeClass: 'active'
+        };
+    },
+
+    getInitialState() {
+        return {
+            visible: false  
+        };
+    },
+
+    componentWillReceiveProps(nextProps) {
+        const {visible} = nextProps;
+
+        if (visible != null) {
+            this.setState({
+                visible
+            });
+        }
+    },
+
+    componentDidMount() {
+        this.componentDidUpdate()
+    },
+
+    componentDidUpdate(prevProps, prevState) {
+        const {visible} = this.state;
+        const {children, activeClass, maskClassName} = this.props;
+        let {getModalContainer} = this.props;
+
+        getModalContainer = getModalContainer || this.getModalContainer;        
+
+        if (this.modalRendered) {
+            let className = children.props.className;
+
+            if (visible) {
+                className = `${className} ${activeClass}`;
+            }
+
+            const modal = React.cloneElement(children, { className });
+
+            renderSubtreeIntoContainer(
+                this,
+                (
+                    <div>
+                        <PinCenter>
+                            {modal}
+                        </PinCenter>
+                        {visible ? <div className={maskClassName}></div> : null}
+                    </div>
+                ),
+                getModalContainer()
+            );
+        }
+    },
+
+    getModalContainer() {
+        let {popupContainer} = this;
+
+        if (popupContainer) {
+            return popupContainer;
+        }
+
+        popupContainer
+        = this.popupContainer 
+        = document.createElement('div');
+
+        document.body.appendChild(popupContainer);
+
+        return popupContainer;
+    },
+
+    render() {
+        const {visible} = this.state;
+
+        this.modalRendered = this.modalRendered || visible;
+
         return (
-            <div>
-                123
-            </div>
+            <noscript />
         );
     }
 });
+
+Modal.propTypes = {
+    visible: PropTypes.bool,
+    maskClassName: PropTypes.string,
+    getModalContainer: PropTypes.func,
+    activeClass: PropTypes.string
+};
 
 export default Modal;
--- test/ModalTest.js
+++ test/ModalTest.js
@@ -1,10 +1,60 @@
 import React from 'react';
+import Modal from '../src/Modal';
 
 const ModalTest = React.createClass({
+    getInitialState() {
+        return {
+            visible: false  
+        };
+    },
+
+    open() {
+        this.setState({
+            visible: true
+        });
+    },
+
+    close() {
+        this.setState({
+            visible: false
+        });
+    },
+
     render() {
+        const {visible} = this.state;
+
         return (
             <div className="modal-test">
                 <h1>Modal</h1>
+                <div 
+                    className="modal-trigger"
+                    onClick={this.open}
+                >
+                    Show Modal
+                </div>
+                <Modal
+                    visible={visible}
+                >
+                    <div className="modal-dialog">
+                        <div className="modal-header">
+                            Tips
+                            <i 
+                                className="icon icon-cross"
+                                onClick={this.close}
+                            >
+                            </i>
+                        </div>
+                        <div className="modal-body">
+                            <h2>Have a nice day, man</h2>
+                            <div 
+                                className="close"
+                                onClick={this.close}
+                            >
+                                Close
+                            </div>
+                        </div>
+                    </div>
+                </Modal>
             </div>
         );
     }
